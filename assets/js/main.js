5fd68e19ea3e1380f77be46dbfa8cf6891aedd04
Joao Lucas <jlucas2088@gmail.com>
2022-10-19 21:27:27 -0300

    tabela funcionando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,145 +1,95 @@
-
-let listaPedidos= [
-  {
-    id: 1001,
-    product: "Super SMACH COMBO Progromado -Hamburguer + Fritas",
-    price: R$55,
-    
-  },
-  {
-      id: 1002,
-      product: "SMACH VariavelBurguer -Hamburguer com bacon",
-      price: R$45,
-      
-  },
-  {
-    id: 1003,
-    product: "SMACH BUG EM PROG - Hambuerguer meio torto",
-    price: R$25,
-    
-  },
-  {
-      id: 1004,
-      product: "Combo Econimico SMACH Char 1 - PAo com carne ",
-      price: R$15,
-      
-  },
-  {
-      id: 1005,
-      product: "Especial SMACH CSS - Hambuguer colorido e alinhado",
-      price: R$65,
-      
-  },
-  {
-    id: 2001,
-    product: "Refrigerante 350ml",
-    price: R$8,
-    
-},
-{
-  id: 2002,
-  product: "Agua 500ml",
-  price: R$5,
-  
-},
-{
-  id: 2003,
-  product: "Suco 350ml",
-  price: R$7,
-  
-},
-{
-  id: 3001,
-  product: "Sorvete 300ml",
-  price: R$15,
-  
-},
-{
-  id: 3002,
-  product: "Sobremesa doce SMACH ARRAY",
-  price: R$50,
-  
-},
-  
-];
-
-let buscar=document.getElementById("buscar2");
-let precoProduto=document.getElementById("precoProduto");
-let codigoProduto=document.getElementById("codigo-produto");
-let produto=document.getElementById('produto');
-let qty=document.getElementById('quantidade');
-let valor=CalculaTotalProduto();
-
 function trocaTela() {
   document.getElementById('page-todos').setAttribute('hidden', true)
   document.getElementById('page-novos').removeAttribute('hidden')
 }
 
+let buscar = document.getElementById('buscar2')
+let preco = document.getElementById('preco')
+let codigoProduto = document.getElementById('codigo-produto')
+let produto = document.getElementById('produto')
+let tbody = document.getElementById('tbody')
+
+let listaProdutos = [
+  {
+    codigo: 1001,
+    product: 'Super SMACH COMBO Progromado -Hamburguer + Fritas',
+    price: 55
+  },
+  {
+    codigo: 1002,
+    product: 'SMACH VariavelBurguer -Hamburguer com bacon',
+    price: 45
+  },
+  {
+    codigo: 1003,
+    product: 'SMACH BUG EM PROG - Hambuerguer meio torto',
+    price: 25
+  },
+  {
+    codigo: 1004,
+    product: 'Combo Econimico SMACH Char 1 - PAo com carne ',
+    price: 15
+  },
+  {
+    codigo: 1005,
+    product: 'Especial SMACH CSS - Hambuguer colorido e alinhado',
+    price: 65
+  },
+  {
+    codigo: 2001,
+    product: 'Refrigerante 350ml',
+    price: 8
+  },
+  {
+    codigo: 2002,
+    product: 'Agua 500ml',
+    price: 5
+  },
+  {
+    codigo: 2003,
+    product: 'Suco 350ml',
+    price: 7
+  },
+  {
+    codigo: 3001,
+    product: 'Sorvete 300ml',
+    price: 15
+  },
+  {
+    codigo: 3002,
+    product: 'Sobremesa doce SMACH ARRAY',
+    price: 50
+  }
+]
+
 function filtrarDadosPorNome() {
-  alert("entrou!!");
-  let filtrados = [];
+  let filtrados = []
 
-  if (buscar.value.toLowerCase() !== "") {
-    filtrados =listaPedidos.filter(pedidos=>pedidos.id == codigoProduto.value);
-  } 
- 
-  //eu preciso filtrar o id,mas o input tem que receber onome do filtrado.Como faz??
-  produto.value=filtrados.product;
-}
-
-/*function listarTabelaProdutos(filtrados = undefined) {
-  console.log('Entrou na função');
-  let trTds = "";
-  let lista = [];
-  let dados = document.getElementById("dados");
-
-  lista = filtrados === undefined ? listaPedidos : filtrados;
-
-  lista.forEach((pedidos) => {
-    trTds += `<tr><td>${pedidos.id}</td>`;
-    trTds += `<td>${pedidos.product}</td>`;
-    trTds += `<td>${pedidos.qty}</td>`;
-    trTds += `<td>${pedidos.valor)}</td>`;??????????
-  });
-  dados.innerHTML = trTds;
-
-  if (lista.length === 0) {
-   dados.innerHTML = `<tr><td colspan="5">Nenhum nome encontrado</td></tr>`;
+  if (buscar.value !== '') {
+    filtrados = listaProdutos.filter(
+      pedidos => pedidos.codigo == codigoProduto.value
+    )
+    console.log(filtrados[0].product)
+    produto.value = filtrados[0].product
+    preco.value = filtrados[0].price
   }
 }
 
-CalculaTotalPedido()
-*/
+function adicionarProutos() {
+  let tr = document.createElement('tr')
 
-//fazer função para adcionar produtos na tabelaProdutos,recebendo dos inputs!
-
-
-function CalculaTotalProduto(total,produtos) {
-  return total + produtos.price*qty;
+  tr.innerHTML = `
+    <td>${codigoProduto.value}</td>
+    <td>${produto.value}</td>
+    <td>${quantidade.value}</td>
+    <td>${preco.value}</td>
+    `
+  tbody.appendChild(tr)
 }
-
-
-
-function CalculaTotalPedido(total,pedido) {
-  return total + pedido.price;
-}
-document.getElementById("demo").innerHTML = listaPedidos.reduce(Compra, 0);
-//so vai funcionar quando tiver produtos listados na tabela!
-
 
 function limparDadosInpBuscar() {
-  codigoProduto.value="";
-  listarTabelaProdutos(listaPedidos);
+  codigoProduto.value = ''
+  listarTabelaProdutos(listaPedidos)
 }
 
-function btnSalvarPedido(){
-  //vai trocar de tela(contrario da primeira troca) e vai adcionar o PEDIDO na tabela
-  document.getElementById('page-novos').setAttribute('hidden', true)
-  document.getElementById('page-todos').removeAttribute('hidden')
-
-}
-
-
-addEventListener("load", () => listarTabelaProdutos());
-
-
+addEventListener('load', () => adicionarProutos())
